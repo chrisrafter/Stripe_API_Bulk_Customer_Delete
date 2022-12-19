9c92bbb4bf1201a354df9b12e73722504559de18
chrisrafter <chrisrafter@gmail.com>
2022-12-19 14:31:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,7 @@
 import stripe
 import time
 stripe.api_key = ""
-cust_list = ["cus_KBqFO4CSOJHszp", "cus_HZbnx0PQV2sD4S", "cus_JZE6VNNTJQS4ej", "cus_JuGmXJAWdOYDj2", "cus_KBpoqSis1TtI4a",
-"cus_KBpWCRYCMv0piL", "cus_KBpvZPl6MR8Z1O", "cus_L1YvLgkBSU340w"]
+cust_list = ["cus_KBqFO4CSOJHszp", "cus_HZbnx0PQV2sD4S"]
 
 #Get the list of customers (API is limited to max of 100 per request)
 r = 1
